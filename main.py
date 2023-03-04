3b38df92d71b1ee8f05b78cd76a3b87cdb8dfdec
Muhyun-Kim <azs030369@gmail.com>
2023-03-04 16:05:47 +0900

    scraper

--- main.py
+++ main.py
@@ -9,5 +9,22 @@ response = get(f"{base_url}{search_term}")
 if response.status_code != 200:
     print("can't request website")
 else:
+    result = []
     soup = BeautifulSoup(response.text, "html.parser")
-    print(soup.find_all('title'))
+    jobs = soup.find_all('section', class_="jobs")
+    for job_section in jobs:
+        job_posts = job_section.find_all('li')
+        job_posts.pop(-1)
+        for post in job_posts:
+            anchors = post.find_all('a')
+            anchor = anchors[1]
+            link = anchor['href']
+            company, kind, region = anchor.find_all('span', class_="company")
+            title = anchor.find('span', class_="title")
+            job_data = {
+                'company':company.string,
+                'region':region.string,
+                'position':title.string,
+            }
+            result.append(job_data)
+    print(result)
